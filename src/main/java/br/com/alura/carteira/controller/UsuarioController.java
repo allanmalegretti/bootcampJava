f18d15b13e4da7f81e9a1af5c82ceca0f4ec3262
Allan M Alegretti <allanalegretti@gmail.com>
2021-11-03 18:40:01 -0300

    Desenvolvendo uma nova funcionalidade na API

---
+++ src/main/java/br/com/alura/carteira/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package br.com.alura.carteira.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.carteira.dto.UsuarioDto;
+import br.com.alura.carteira.dto.UsuarioFormDto;
+import br.com.alura.carteira.modelo.Usuario;
+import br.com.alura.carteira.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService service;
+
+	@GetMapping
+	public List<UsuarioDto> listar() {
+		return service.Listar();
+	}
+
+	@PostMapping
+	public void cadastrar(@RequestBody @Valid UsuarioFormDto dto) {
+		service.cadastrar(dto);
+	}
+}
