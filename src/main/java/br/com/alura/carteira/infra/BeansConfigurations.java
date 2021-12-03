f09b71f89dcab6f1bb058785d88b0a6425e6fafb
Allan M Alegretti <allanalegretti@gmail.com>
2021-12-03 18:13:11 -0300

    Autenticação Stateless via Token

--- src/main/java/br/com/alura/carteira/infra/BeansConfigurations.java
+++ src/main/java/br/com/alura/carteira/infra/BeansConfigurations.java
@@ -3,6 +3,7 @@ package br.com.alura.carteira.infra;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
--- src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
@@ -3,9 +3,13 @@ package br.com.alura.carteira.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -18,6 +22,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(autenticacaoService).passwordEncoder(bCryptPasswordEncoder);
 	}
@@ -25,12 +35,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.anyRequest().authenticated()
-		.and().formLogin()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().csrf().disable();
 	}
 	
-//	public static void main(String[] args) {
-//		System.out.println(new BCryptPasswordEncoder().encode("123456"));
-//	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+	}
+	
 }
