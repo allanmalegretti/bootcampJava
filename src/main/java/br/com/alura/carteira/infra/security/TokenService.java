07e73b16b32cb4abbe0d15255c4a3d97e8018b03
Allan M Alegretti <allanalegretti@gmail.com>
2021-12-08 23:58:24 -0300

    Controle de acesso com token

--- src/main/java/br/com/alura/carteira/infra/security/TokenService.java
+++ src/main/java/br/com/alura/carteira/infra/security/TokenService.java
@@ -5,21 +5,43 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.carteira.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class TokenService {
-	
+
 	@Value("${jjwt.secret}")
 	private String secret;
-	
+
 	public String gerarToken(Authentication authentication) {
 		Usuario logado = (Usuario) authentication.getPrincipal();
 		return Jwts
 				.builder()
-				.setId(logado.getId().toString())
+				.setSubject(logado.getId().toString())
 				.signWith(SignatureAlgorithm.HS256, secret)
 				.compact();
 	}
+
+	public boolean isValido(String token) {
+		try {
+			Jwts
+			.parser()
+			.setSigningKey(secret)
+			.parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Long extrairIdUsuario(String token) {
+			Claims claims = Jwts
+			.parser()
+			.setSigningKey(secret)
+			.parseClaimsJws(token)
+			.getBody();
+		return Long.parseLong(claims.getSubject());
+	};
 }
